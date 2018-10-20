4a8579098e28b737cd90f69070d76271b20d0012
Ismael <karma@MacBook-Air-de-Ismael.local>
2018-10-20 21:01:39 +0200

    lol

--- script1.bash
+++ script1.bash
@@ -5,6 +5,6 @@ do
 	if [ -f $file ] || [ -d $file ]; then
 		#!echo "ok il s'agit d'un ficher ou d'un dossier"
 		echo -e "Nom du fichier $file"
-		ls -l $file |grep '^.r' | cut -d' ' -f1 
+		ls -l $file | cut -d' ' -f1 | cut -c 2-10
 	fi
 done
